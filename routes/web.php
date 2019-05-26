48375f00015cdfd1989248c4b4994e6bbf8a2f85
Jozo <j.spaajic@gmail.com>
2019-05-26 19:57:09 +0200

    metoda na logout promijenjena iz POST u GET!

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ Route::group(['middleware' => ['web']], function (){
     //Auth rute
     Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
     Route::post('login', 'Auth\LoginController@login');
-    Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    Route::get('logout', 'Auth\LoginController@logout')->name('logout');
     //register  rute
     Route::get('register', 'Auth\RegisterController@showRegistrationForm')->name('register');
     Route::post('register', 'Auth\RegisterController@register');
